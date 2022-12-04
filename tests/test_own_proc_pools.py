55ec56c62a88dba7b1843e1f8fb995223e531bf7
Martin Dočekal <docekmartin@gmail.com>
2022-12-04 16:53:52 +0100

    multiprocessing pool using thread multiprocessing pool with workers factory

--- tests/test_own_proc_pools.py
+++ tests/test_own_proc_pools.py
@@ -7,11 +7,13 @@ Created on 04.12.21
 import multiprocessing
 import os
 import unittest
+import math
 from multiprocessing.context import BaseContext
 from multiprocessing.queues import Queue
 from typing import List
 
-from windpyutils.parallel.own_proc_pools import FunctorPool, FunctorWorker, BaseFunctorWorker
+from windpyutils.parallel.own_proc_pools import FunctorPool, FunctorWorker, BaseFunctorWorker, FunctorWorkerFactory, \
+    FactoryFunctorPool
 
 context_fork = multiprocessing.get_context("fork")
 context_spawn = multiprocessing.get_context("spawn")
@@ -20,8 +22,8 @@ context_forkserver = multiprocessing.get_context("forkserver")
 
 class BaseMockWorker(BaseFunctorWorker):
 
-    def __init__(self, context: BaseContext = multiprocessing.get_context()):
-        super().__init__(context)
+    def __init__(self, context: BaseContext = multiprocessing.get_context(), max_chunks_per_worker: float = math.inf):
+        super().__init__(context, max_chunks_per_worker)
         self.scaler = None
         self.begin_called = context.Event()
         self.end_called = context.Event()
@@ -37,8 +39,8 @@ class BaseMockWorker(BaseFunctorWorker):
 
 
 class MockWorker(FunctorWorker):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, max_chunks_per_worker: float = math.inf):
+        super().__init__(max_chunks_per_worker)
         self.scaler = None
         self.begin_called = multiprocessing.Event()
         self.end_called = multiprocessing.Event()
@@ -54,23 +56,23 @@ class MockWorker(FunctorWorker):
 
 
 class ForkMockWorker(BaseMockWorker, context_fork.Process):
-    def __init__(self):
-        super().__init__(context_fork)
+    def __init__(self, max_chunks_per_worker: float = math.inf):
+        super().__init__(context_fork, max_chunks_per_worker)
 
 
 class SpawnMockWorker(BaseMockWorker, context_spawn.Process):
-    def __init__(self):
-        super().__init__(context_spawn)
+    def __init__(self, max_chunks_per_worker: float = math.inf):
+        super().__init__(context_spawn, max_chunks_per_worker)
 
 
 class ForkServerMockWorker(BaseMockWorker, context_forkserver.Process):
-    def __init__(self):
-        super().__init__(context_forkserver)
+    def __init__(self, max_chunks_per_worker: float = math.inf):
+        super().__init__(context_forkserver, max_chunks_per_worker)
 
 
 class BaseMockWorkerLargeData(BaseFunctorWorker):
-    def __init__(self, context: BaseContext = multiprocessing.get_context()):
-        super().__init__(context)
+    def __init__(self, context: BaseContext = multiprocessing.get_context(), max_chunks_per_worker: float = math.inf):
+        super().__init__(context, max_chunks_per_worker)
         self.scaler = None
         self.begin_called = context.Event()
         self.end_called = context.Event()
@@ -86,8 +88,8 @@ class BaseMockWorkerLargeData(BaseFunctorWorker):
 
 
 class MockWorkerLargeData(FunctorWorker):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, max_chunks_per_worker: float = math.inf):
+        super().__init__(max_chunks_per_worker)
         self.scaler = None
         self.begin_called = multiprocessing.Event()
         self.end_called = multiprocessing.Event()
@@ -102,19 +104,32 @@ class MockWorkerLargeData(FunctorWorker):
         self.end_called.set()
 
 
+class MockFunctorWorkerFactory(FunctorWorkerFactory):
+
+    def __init__(self, worker_cls=MockWorker, max_chunks_per_worker: float = math.inf):
+        self.created_workers = []
+        self.worker_cls = worker_cls
+        self.max_chunks_per_worker = max_chunks_per_worker
+
+    def create(self) -> BaseFunctorWorker:
+        w = self.worker_cls(self.max_chunks_per_worker)
+        self.created_workers.append(w)
+        return w
+
+
 class ForkMockWorkerLargeData(BaseMockWorkerLargeData, context_fork.Process):
-    def __init__(self):
-        super().__init__(context_fork)
+    def __init__(self, max_chunks_per_worker: float = math.inf):
+        super().__init__(context_fork, max_chunks_per_worker)
 
 
 class SpawnMockWorkerLargeData(BaseMockWorkerLargeData, context_spawn.Process):
-    def __init__(self):
-        super().__init__(context_spawn)
+    def __init__(self, max_chunks_per_worker: float = math.inf):
+        super().__init__(context_spawn, max_chunks_per_worker)
 
 
 class ForkServerMockWorkerLargeData(BaseMockWorkerLargeData, context_forkserver.Process):
-    def __init__(self):
-        super().__init__(context_forkserver)
+    def __init__(self, max_chunks_per_worker: float = math.inf):
+        super().__init__(context_forkserver, max_chunks_per_worker)
 
 
 class TestFunctorPool(unittest.TestCase):
@@ -236,6 +251,102 @@ class TestForkServerFunctorPool(TestFunctorPool):
         self.workers = [ForkServerMockWorker() for _ in range(2)]
         self._large_worker_class = ForkServerMockWorkerLargeData
         self.context = context_forkserver
+
+
+class TestFactoryFunctorPool(unittest.TestCase):
+    def setUp(self) -> None:
+        self.workers = 2
+        self.factory = MockFunctorWorkerFactory()
+        self.context = multiprocessing.get_context()
+
+    def test_init(self):
+        if os.cpu_count() > 1:
+
+            with FactoryFunctorPool(self.workers, self.factory, self.context):
+                self.assertEqual(2, len(self.factory.created_workers))
+                for i, w in enumerate(self.factory.created_workers):
+                    self.assertEqual(i, w.wid)
+                    self.assertTrue(isinstance(w.work_queue, Queue))
+                    self.assertTrue(isinstance(w.results_queue, Queue))
+                    self.assertTrue(isinstance(w.replace_queue, Queue))
+
+            for w in self.factory.created_workers:
+                self.assertTrue(w.begin_called.is_set())
+                self.assertTrue(w.end_called.is_set())
+        else:
+            self.skipTest("This test can only be run on the multi cpu device.")
+
+    def test_imap(self):
+        if os.cpu_count() > 1:
+            data = [i for i in range(10000)]
+            with FactoryFunctorPool(self.workers, self.factory, self.context) as pool:
+                results = list(pool.imap(data))
+
+            self.assertListEqual([i * 2 for i in data], results)
+
+            for w in self.factory.created_workers:
+                self.assertTrue(w.begin_called.is_set())
+                self.assertTrue(w.end_called.is_set())
+        else:
+            self.skipTest("This test can only be run on the multi cpu device.")
+
+    def test_imap_large_data(self):
+        if os.cpu_count() > 1:
+            self.factory.worker_cls = MockWorkerLargeData
+            data = [i for i in range(10000)]
+
+            with FactoryFunctorPool(self.workers, self.factory, self.context) as pool:
+                results = list(x[-1] for x in pool.imap(data))
+                self.assertListEqual([i * 2 for i in data], results)
+
+            for w in self.factory.created_workers:
+                self.assertTrue(w.begin_called.is_set())
+                self.assertTrue(w.end_called.is_set())
+        else:
+            self.skipTest("This test can only be run on the multi cpu device.")
+
+    def test_map_chunk_size(self):
+        if os.cpu_count() > 1:
+            data = [i for i in range(10000)]
+            with FactoryFunctorPool(self.workers, self.factory, self.context) as fm:
+                results = list(fm.imap(data, chunk_size=250))
+                self.assertListEqual(results, [i * 2 for i in data])
+
+            for w in self.factory.created_workers:
+                self.assertTrue(w.begin_called.is_set())
+                self.assertTrue(w.end_called.is_set())
+        else:
+            self.skipTest("This test can only be run on the multi cpu device.")
+
+    def test_map_chunk_big_size(self):
+        if os.cpu_count() > 1:
+            data = [i for i in range(10000)]
+
+            with FactoryFunctorPool(self.workers, self.factory, self.context) as fm:
+                results = list(fm.imap(data, chunk_size=700))
+                self.assertListEqual(results, [i * 2 for i in data])
+
+            for w in self.factory.created_workers:
+                self.assertTrue(w.begin_called.is_set())
+                self.assertTrue(w.end_called.is_set())
+        else:
+            self.skipTest("This test can only be run on the multi cpu device.")
+
+    def test_replace(self):
+        if os.cpu_count() > 1:
+            data = [i for i in range(10000)]
+            self.factory.max_chunks_per_worker = 10
+            with FactoryFunctorPool(self.workers, self.factory, self.context) as fm:
+                results = list(fm.imap(data, chunk_size=100))
+                self.assertListEqual(results, [i * 2 for i in data])
+
+            self.assertEqual(12, len(self.factory.created_workers))
+            # len(data)/1000 + 2(this two will only receive stop signal) -> 12
+            for w in self.factory.created_workers:
+                self.assertTrue(w.begin_called.is_set())
+                self.assertTrue(w.end_called.is_set())
+        else:
+            self.skipTest("This test can only be run on the multi cpu device.")
 
 
 if __name__ == '__main__':
